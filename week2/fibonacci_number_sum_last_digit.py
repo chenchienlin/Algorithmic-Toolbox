e47bf43da86e3e5ece0fa7dcc86ad1b478037211
x91003502 <x91003502@gmail.com>
2021-06-15 20:55:01 +0800

    Added fibonacci_number_sum_last_digit.py

--- week2/fibonacci_number_sum_last_digit.py
+++ week2/fibonacci_number_sum_last_digit.py
@@ -1,26 +1,37 @@
-def iter_fib_last_digit(n):
+def iter_fib_sum_last_digit(n):
     if n <= 1:
         return n
     else:
         a, b = 0, 1
+        
+        sum = 0
+        sum += b
         for i in range(2, n+1):
             temp = b
             b = (a + b)%10
             a = temp%10
-        return b
+            
+            sum += b
+        return sum
 
 import math
 import numpy as np
-def matrix_fib_last_digit(n):
+def matrix_fib_sum_last_digit(n):
     '''
         Assume matrix multiplication is O(1) and % operator is O(1), the time complexity is O(logn).
         Because there is only one digit in each entry of the matrix, there is no overhead for two 
         big numbers multiplication.
     '''
+    if n <= 1: return n
+    
     m = math.floor(n/2)
     M = np.matrix([[0, 1], [1, 1]])
     M2 = M ** 2
     V = np.matrix([[1, 0]]).T
+    result = M * V
+    last_digit_sum = 0
+    last_digit_sum += result[0][0,0] % 10
+    last_digit_sum += result[1][0,0] % 10
     for i in range(0, m):
         M = M * M2
         
@@ -35,9 +46,11 @@ def matrix_fib_last_digit(n):
         M[1,0] = M[1,0] % 10
         M[1,1] = M[1,1] % 10
         
-        
-    result = M * V
+        result = M * V
+        last_digit_sum += result[0][0,0] % 10
+        last_digit_sum += result[1][0,0] % 10
+    
     if n == 2*m:
-        return result[0][0,0] % 10
+        return last_digit_sum - result[1][0,0] % 10
     elif n == 2*m+1:
-        return result[1][0,0] % 10
+        return last_digit_sum
