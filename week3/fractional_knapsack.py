ebd7def3ede9581748eeb3864a702ca29e132499
x91003502 <x91003502@gmail.com>
2021-06-16 23:29:01 +0800

    Added fractional_knapsack_sort function.

--- week3/fractional_knapsack.py
+++ week3/fractional_knapsack.py
@@ -28,7 +28,38 @@ def fractional_knapsack(W, wn, vn):
         wn[best] = wn[best] - a
         amount[best] = amount[best] + a
 
+# wn = [4, 3, 2]
+# vn = [20, 18, 14]
+# W = 7
+# fractional_knapsack(W, wn, vn)
+
+def fractional_knapsack_sort(W, wn, vn):
+    frac = list()
+    amount = dict() # store the amount of items which are taken
+    for i in range(0, len(vn)):
+        frac.append(vn[i]/wn[i])
+        amount[str(vn[i])] = 0
+    
+    zip_lists = zip(frac, wn, vn)
+    sorted_zip_lists = sorted(zip_lists, reverse=True)
+    frac, wn, vn = zip(*sorted_zip_lists) # zip return tuple and tuple cannot do assignment
+    frac, wn, vn = list(frac), list(wn), list(vn)
+    
+    idx = 0
+    V = 0
+    while W > 0:
+        a = min(W, wn[idx])
+        
+        W = W - a
+        wn[idx] = wn[idx] - a
+        
+        amount[str(vn[idx])] = a
+        
+        V = V + a * frac[idx]
+        
+        idx += 1
+
 wn = [4, 3, 2]
 vn = [20, 18, 14]
 W = 7
-fractional_knapsack(W, wn, vn)
+fractional_knapsack_sort(W, wn, vn)
