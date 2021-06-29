65b8e23fd834994d0be8e04d89adafe638700667
x91003502 <x91003502@gmail.com>
2021-06-29 23:34:47 +0800

    Added 3 way partition and test cases.

--- week4/parition.py
+++ week4/parition.py
@@ -8,6 +8,20 @@ def partition(arr, lo, hi):
     swap(arr, lo, j)
     return j
 
+def partition3(arr, lo, hi):
+    p = arr[lo]
+    j = lo
+    k = lo
+    for i in range(lo+1, hi+1):
+        if p > arr[i]:
+            j += 1
+            k += 1
+            swap(arr, i, j)
+        elif p == arr[i]:
+            j += 1
+    swap(arr, lo, j)
+    return k, j
+
 def swap(arr, i, j):
     temp = arr[i]
     arr[i] = arr[j]
--- week4/test_parition.py
+++ week4/test_parition.py
@@ -14,3 +14,28 @@ def test_partition():
         lo = 0
         hi = len(arr) - 1
         assert partition(arr, lo, hi) == pivot
+
+def test_partition3_1():
+    arr = [3,8,9,2,5,3,4,3,1,7,0,6]
+    lo = 0
+    hi = len(arr) - 1
+    low, high = partition3(arr, lo, hi)
+    assert low == 3 and high == 5
+
+def test_partition3_2():
+    iter = 100
+    for _ in range(iter):
+        n = 100
+        arr = [i for i in range(n)]
+        random.shuffle(arr)
+        pivot = arr[0]
+        
+        repeat = 3
+        for _ in range(repeat):
+            arr.append(pivot)
+        
+        lo = 0
+        hi = len(arr) - 1
+        low, high = partition3(arr, lo, hi)
+        LOGGER.debug(f'Pivot element : {pivot}')
+        assert low == pivot and high == low + repeat
