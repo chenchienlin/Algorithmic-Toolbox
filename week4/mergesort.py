b5c367087bea69fad7b61e709ea716aa65886c73
x91003502 <x91003502@gmail.com>
2021-06-30 23:25:07 +0800

    Created mergesort.py and test_mergesort.py.

---
+++ week4/mergesort.py
@@ -0,0 +1,26 @@
+import math
+import logging
+from week4.merge import merge
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger()
+
+def mergesort(arr):
+    aux = arr.copy()
+    lo = 0
+    hi = len(arr) - 1
+    recur_mergesort(arr, aux, lo, hi)
+
+def recur_mergesort(arr, aux, lo, hi):
+    if hi > lo:
+        mid = int(math.floor( (lo+hi)/2 ))
+        recur_mergesort(arr, aux, lo, mid)
+        recur_mergesort(arr, aux, mid+1, hi)
+        merge(arr, aux, lo, mid, hi)
+
+import random
+arr = [5,6,7,8,1,2,3,4]
+aux = arr.copy()
+lo = 0
+hi = len(arr) - 1
+
+# mergesort(arr)
---
+++ week4/test_mergesort.py
@@ -0,0 +1,33 @@
+import random
+import logging
+from week4.mergesort import *
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger()
+
+def test_mergesort1():
+    arr = [5,6,7,8,1,2,3,4]
+    mergesort(arr)
+    LOGGER.debug(arr)
+    assert arr == sorted(arr)
+
+def test_mergesort2():
+    lo_range = 0
+    hi_range = 100
+    n = 100
+    iter = 100
+    for _ in range(iter):
+        arr = sorted([random.randint(lo_range, hi_range) for _ in range(n)])
+        mergesort(arr)
+        LOGGER.debug(arr)
+        assert arr == sorted(arr)
+
+def test_mergesort3():
+    lo_range = -100
+    hi_range = 100
+    n = 200
+    iter = 100
+    for _ in range(iter):
+        arr = sorted([random.randint(lo_range, hi_range) for _ in range(n)])
+        mergesort(arr)
+        LOGGER.debug(arr)
+        assert arr == sorted(arr)
