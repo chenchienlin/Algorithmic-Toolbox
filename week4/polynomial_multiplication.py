b62625975f1bff00ba7c1400552484d725581e7e
x91003502 <x91003502@gmail.com>
2021-07-04 19:36:39 +0800

    Updated polynomial_multiplication.py. Added recursive implementation.

--- week4/polynomial_multiplication.py
+++ week4/polynomial_multiplication.py
@@ -1,6 +1,32 @@
+import math
+import logging
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger()
+
 def polynomial_multiplication_naive(p1, p2):
     result = [0 for _ in range( len(p1) + len(p2) -1 )]
     for i in range(len(p1)):
         for j in range(len(p2)):
             result[i+j] += p1[i] * p2[j]
     return result
+
+# works when p1 and p2 have even number of terms
+def polynomial_multiplication(p1, p2, n, al, bl):
+    res = [0 for _ in range(0, 2*n-1)]
+    if n == 1:
+        res[0] = p1[al] * p2[bl]
+        return res
+    n2 = math.ceil(n/2)
+    res[0:n-2 + 1] = polynomial_multiplication(p1, p2, n2, al, bl)
+    res[n:2*n-2 + 1] = polynomial_multiplication(p1, p2,n2, al + n2, bl + n2)
+    D0E0 = polynomial_multiplication(p1, p2, n2, al, bl)
+    D1E1 = polynomial_multiplication(p1, p2,n2, al + n2, bl + n2)
+    D0E1 = polynomial_multiplication(p1, p2, n2, al, bl + n2)
+    D1E0 = polynomial_multiplication(p1, p2, n2, al + n2, bl)
+    LOGGER.debug(f'D0E0 {D0E0}')
+    LOGGER.debug(f'D1E1 {D1E1}')
+    LOGGER.debug(f'D0E1 {D0E1}')
+    LOGGER.debug(f'D1E0 {D1E0}')
+    res[n2:n+n2-2 + 1] = [a + b for a, b in zip(res[n2:n+n2-2 + 1], D0E1)]
+    res[n2:n+n2-2 + 1] = [a + b for a, b in zip(res[n2:n+n2-2 + 1], D1E0)]
+    return res
--- week4/test_polynomial_multiplication.py
+++ week4/test_polynomial_multiplication.py
@@ -3,7 +3,15 @@ from week4.polynomial_multiplication import *
 logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger()
 
-def test_polynomial_multiplication1():
+def test_polynomial_multiplication_naive():
     p1 = [3,2,5]
     p2 = [5,1,2]
     assert polynomial_multiplication_naive(p1, p2) == [15,13,33,9,10]
+
+def test_polynomial_multiplication():
+    p1 = [1,2,3,4,5,6,7,8]
+    p2 = [8,7,6,5,4,3,2,1]
+    n = len(p1)
+    al = 0
+    bl = 0
+    assert polynomial_multiplication(p1, p2, n, al, bl) == polynomial_multiplication_naive(p1, p2)
