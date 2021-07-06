d9dceef42987f22fa8eb4a0454de312dcf07f263
x91003502 <x91003502@gmail.com>
2021-07-06 19:48:20 +0800

    Added n_queens function. Added test case for

--- backtracking/n_queens.py
+++ backtracking/n_queens.py
@@ -1,3 +1,7 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger()
+
 def detect_attack(prev_row, prev_col, curr_row):
     diff = curr_row - prev_row
     return [prev_col-diff, prev_col, prev_col+diff]
@@ -8,24 +12,16 @@ def detect_all_attack(Q, curr_row):
         prev_col = Q[prev_row]
         invalid = detect_attack(prev_row, prev_col, curr_row)
         for i in invalid:
-            if i > 0 or i <= len(Q)-1:
+            if i >= 0 and i <= len(Q)-1:
                 all_invalid.append(i)
     return all_invalid
 
-prev_row = 0
-prev_col = 4
-curr_row = 1
-invalid = detect_attack(prev_row, prev_col, curr_row)
-print(invalid)
-
-curr_row = 2
-invalid = detect_attack(prev_row, prev_col, curr_row)
-print(invalid)
-
-
-Q = [0 for _ in range(4)]
-Q[0] = 1
-Q[1] = 3
-curr_row = 2
-all_invalid = detect_all_attack(Q, curr_row)
-print(all_invalid)
+def n_queens(Q, curr_row, ans):
+    if curr_row >= len(Q):
+        ans.append(Q.copy())
+        LOGGER.debug(Q)
+    all_invalid = detect_all_attack(Q, curr_row)
+    for col in range(len(Q)): # col=0:len(Q)-1
+        if col not in all_invalid:
+            Q[curr_row] = col
+            n_queens(Q, curr_row+1, ans)
--- backtracking/test_n_queens.py
+++ backtracking/test_n_queens.py
@@ -60,3 +60,21 @@ def test_detect_all_attack2():
     assert 0 in all_invalid
     assert 1 in all_invalid
     assert 3 in all_invalid
+
+def test_n_queens():
+    #   0   1   2   3      0   1   2   3  
+    # -----------------  -----------------
+    # |   | Q |   |   |  |   |   | Q |   |
+    # -----------------  -----------------
+    # |   |   |   | Q |  | Q |   |   |   |
+    # -----------------  -----------------
+    # | Q |   |   |   |  |   |   |   | Q |
+    # -----------------  -----------------
+    # |   |   | Q |   |  |   | Q |   |   |
+    # -----------------  -----------------
+    ans = []
+    Q = [0 for i in range(4)]
+    curr_row = 0
+    n_queens(Q, curr_row, ans)
+    assert [1,3,0,2] in ans
+    assert [2,0,3,1] in ans
