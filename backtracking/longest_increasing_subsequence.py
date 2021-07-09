fd736434019ffc4628c7f1acc6df0b2ea223f9c7
x91003502 <x91003502@gmail.com>
2021-07-09 21:21:19 +0800

    Added another brute force recursive implementation for longest increasing subsequence.

--- backtracking/longest_increasing_subsequence.py
+++ backtracking/longest_increasing_subsequence.py
@@ -29,3 +29,23 @@ def construct_longest_increasing_subsequence(seq, i, curr_max):
             return a if len(a) > len(b) else b
         else:
             return b
+
+def longest_increasing_subsequence2(seq):
+    best = 0
+    for i in range(len(seq)):
+        a = recur_increasing_subsequence(seq, i)
+        if a > best:
+                best = a
+    return best
+
+def recur_increasing_subsequence(seq, i):
+    if i == len(seq):
+        return 1
+    else:
+        best = 0
+        for j in range(i+1, len(seq)):
+            if seq[j] >= seq[i]:
+                a = recur_increasing_subsequence(seq, j)
+                if a > best:
+                    best = a
+        return best + 1
--- backtracking/test_longest_increasing_subsequence.py
+++ backtracking/test_longest_increasing_subsequence.py
@@ -7,19 +7,23 @@ def test_longest_increasing_subsequence1():
     seq = [3,10,2,1,20]
     i = 0
     curr_max = float('-inf')
-    longest_increasing_subsequence(seq, i, curr_max) == 3
+    assert longest_increasing_subsequence(seq, i, curr_max) == 3
+    assert longest_increasing_subsequence2(seq) == 3
 
 def test_longest_increasing_subsequence2():
     seq = [3,2]
     i = 0
     curr_max = float('-inf')
-    longest_increasing_subsequence(seq, i, curr_max) == 1
+    assert longest_increasing_subsequence(seq, i, curr_max) == 1
+    assert longest_increasing_subsequence2(seq) == 1
+    
 
 def test_longest_increasing_subsequence3():
     seq = [50,3,10,7,40,80]
     i = 0
     curr_max = float('-inf')
-    longest_increasing_subsequence(seq, i, curr_max) == 4
+    assert longest_increasing_subsequence(seq, i, curr_max) == 4
+    assert longest_increasing_subsequence2(seq) == 4
 
 def test_construct_longest_increasing_subsequence1():
     seq = [3,10,2,1,20]
