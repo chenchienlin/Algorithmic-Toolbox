149b5ea2d85c8086dfd35e10bee32a603ef250cf
x91003502 <x91003502@gmail.com>
2021-08-09 23:39:45 +0800

    Updated n_queue.py. (Use set instead of list when computing all invalid).

--- backtracking/n_queens.py
+++ backtracking/n_queens.py
@@ -7,14 +7,12 @@ def detect_attack(prev_row, prev_col, curr_row):
     return [prev_col-diff, prev_col, prev_col+diff]
 
 def detect_all_attack(Q, curr_row):
-    all_invalid = []
+    all_invalid = set()
     for prev_row in range(curr_row): # prev_row=0:curr_row-1
         prev_col = Q[prev_row]
         invalid = detect_attack(prev_row, prev_col, curr_row)
-        for i in invalid:
-            if i >= 0 and i <= len(Q)-1:
-                all_invalid.append(i)
-    return all_invalid
+        all_invalid.update(invalid)
+    return list(all_invalid)
 
 def n_queens(Q, curr_row, ans):
     if curr_row >= len(Q):
