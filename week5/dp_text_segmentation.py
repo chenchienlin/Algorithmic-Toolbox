25deb07e0c785e944204c178f21b2b43e228cebe
chenchienlin <x91003502@gmail.com>
2021-09-03 18:41:30 +0800

    Added dp_splitable function and test cases.

--- week5/dp_text_segmentation.py
+++ week5/dp_text_segmentation.py
@@ -5,7 +5,7 @@ LOGGER = logging.getLogger()
 def is_word(string, dictionary):
     return string in dictionary
 
-def dp_splitable(S, i, dictionary, memo):
+def memo_splitable(S, i, dictionary, memo):
     if i == len(S):
         return True
     for j in range(i, len(S)):
@@ -15,7 +15,25 @@ def dp_splitable(S, i, dictionary, memo):
             if j+1 in memo and memo[j+1] is True:
                 return True
             else:
-                memo[j+1] = dp_splitable(S, j+1, dictionary, memo)
+                memo[j+1] = memo_splitable(S, j+1, dictionary, memo)
                 if memo[j+1] is True:
                     return True
     return False
+
+def dp_splitable(S, dictionary):
+    # Init memoization data structure
+    table = [None for _ in range(len(S)+1)]
+    
+    # Compute recurrence base case
+    table[len(S)] = True
+    
+    # Compute from i=len(S)-1:0 j=i:len(S)-1
+    j = len(S) - 1
+    for i in reversed(range(len(S))):
+        for j in range(i, len(S)):
+            substr = S[i:j+1]
+            if is_word(substr, dictionary) and table[j+1]:
+                table[i] = True
+                break
+            table[i] = False
+    return table[0]
--- week5/test_dp_text_segmentation.py
+++ week5/test_dp_text_segmentation.py
@@ -3,31 +3,36 @@ from week5.dp_text_segmentation import *
 logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger()
 
-def test_dp_splitable1():
+def test_memo_splitable1():
     dictionary = {  'BOT', 'BOTH', 'THE', 'HE', 'HEAR', 'HEART', 'HEARTH', 
                     'EARTH', 'ART', 'HAND', 'HANDS', 'AND', 'SATURN', 'AT',
                     'TURN', 'TURNS', 'URN', 'URNS', 'SPIN', 'PIN'}
     S = 'BOTH'
     i = 0
     memo = dict()
-    assert dp_splitable(S, i, dictionary, memo) == True
+    assert memo_splitable(S, i, dictionary, memo) == True
+    assert dp_splitable(S, dictionary) == True
     
     S = 'BOTHEARTH'
     i = 0
     memo = dict()
-    assert dp_splitable(S, i, dictionary, memo) == True
+    assert memo_splitable(S, i, dictionary, memo) == True
+    assert dp_splitable(S, dictionary) == True
     
     S = 'BOTHEARTHE'
     i = 0
     memo = dict()
-    assert dp_splitable(S, i, dictionary, memo) == True
+    assert memo_splitable(S, i, dictionary, memo) == True
+    assert dp_splitable(S, dictionary) == True
     
     S = 'BOTHEARTHANDSATURNSPIN'
     i = 0
     memo = dict()
-    assert dp_splitable(S, i, dictionary, memo) == True
+    assert memo_splitable(S, i, dictionary, memo) == True
+    assert dp_splitable(S, dictionary) == True
     
     S = 'SATURNSPIN'
     i = 0
     memo = dict()
-    assert dp_splitable(S, i, dictionary, memo) == True
+    assert memo_splitable(S, i, dictionary, memo) == True
+    assert dp_splitable(S, dictionary) == True
