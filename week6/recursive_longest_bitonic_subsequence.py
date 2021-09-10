47dab760c4ead54f576394f9b433a0d09b53b9e4
x91003502 <x91003502@gmail.com>
2021-09-10 20:58:19 +0800

    Re-implemented recursive_longest_bitonic_subsequence function.

--- week6/recursive_longest_bitonic_subsequence.py
+++ week6/recursive_longest_bitonic_subsequence.py
@@ -2,43 +2,37 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger()
 
-def longest_decreasing_subsequence(seq):
-    seq_copy = seq.copy()
-    seq_copy.insert(0, float('inf'))
-    return recur_LDS(seq_copy, i=0, j=1)
-
-def recur_LDS(seq, i, j):
-    if j == len(seq):
-        return 0
-    elif seq[i] <= seq[j]:
-        return recur_LDS(seq, i, j+1)
-    else:
-        # include j
-        a = recur_LDS(seq, j, j+1) + 1
-        
-        # exclude j
-        b = recur_LDS(seq, i, j+1)
-        return max(a, b)
-
 def recursive_longest_bitonic_subsequence(seq):
     seq_copy = seq.copy()
     seq_copy.insert(0, float('-inf'))
-    return recur_LBS(seq_copy, i=0, j=1)
+    best = 0
+    for k in range(1, len(seq_copy)):
+        a = recur_LIS(seq_copy, 0, 1, k+1) + recur_LDS(seq_copy, k, k+1, len(seq_copy))
+        best = max(a, best)
+    return best
 
-def recur_LBS(seq, i, j):
-    if j == len(seq):
+def recur_LIS(seq, i, j, k):
+    if j == k:
         return 0
     elif seq[i] >= seq[j]:
-        # skip
-        a = recur_LBS(seq, i, j+1)
-        # find decreasing
-        b = recur_LDS(seq, i, j)
-        return max(a, b)
+        return recur_LIS(seq, i, j+1, k)
     else:
-        # skip
-        a = recur_LBS(seq, i, j+1)
-        # find decreasing
-        b = recur_LDS(seq, i, j)
-        # take
-        c = recur_LBS(seq, j, j+1) + 1
-        return max(a, b, c)
+        # include j
+        a = recur_LIS(seq, j, j+1, k) + 1
+        
+        # exclude j
+        b = recur_LIS(seq, i, j+1, k)
+        return max(a, b)
+
+def recur_LDS(seq, i, j, k):
+    if j == k:
+        return 0
+    elif seq[i] <= seq[j]:
+        return recur_LDS(seq, i, j+1, k)
+    else:
+        # include j
+        a = recur_LDS(seq, j, j+1, k) + 1
+        
+        # exclude j
+        b = recur_LDS(seq, i, j+1, k)
+        return max(a, b)
